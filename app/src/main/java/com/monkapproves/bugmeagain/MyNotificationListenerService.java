1a51b71738e341466ea049e8f963d4ddab6c7dd6
Saurabh Gupta <saurabgu@adobe.com>
2016-12-13 17:07:33 +0530

    First working version. Switch to Priority mode or more intrusive to get all notifications removed. Click on List of Notification to see them all back.

--- app/src/main/java/com/monkapproves/bugmeagain/MyNotificationListenerService.java
+++ app/src/main/java/com/monkapproves/bugmeagain/MyNotificationListenerService.java
@@ -1,15 +1,19 @@
 package com.monkapproves.bugmeagain;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Random;
@@ -18,12 +22,32 @@ import java.util.Set;
 public class MyNotificationListenerService extends NotificationListenerService {
     private String TAG = this.getClass().getSimpleName();
     private Set<StatusBarNotification> snoozedNotifs = new HashSet<>();
+
+    private NLServiceReceiver nlservicereciver;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        nlservicereciver = new NLServiceReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.monkapproves.bugmelater.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+        registerReceiver(nlservicereciver,filter);
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(nlservicereciver);
+    }
+
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-
-        Log.i(TAG, "**********  onNotificationPosted");
-        snoozedNotifs.add(sbn);
-        cancelNotification(sbn.getKey());
+        if(sbn.getNotification().extras.getChar("replayed") == 'Y') {
+            Log.i(TAG, "can't touch this.");
+            return;
+        }
+        if(getCurrentInterruptionFilter() > NotificationManager.INTERRUPTION_FILTER_ALL) {
+            snoozedNotifs.add(sbn);
+            cancelNotification(sbn.getKey());
+        }
     }
 
     @Override
@@ -39,28 +63,25 @@ public class MyNotificationListenerService extends NotificationListenerService {
         Iterator<StatusBarNotification> notifs = snoozedNotifs.iterator();
         while(notifs.hasNext())
         {
-            mNotifyMgr.notify(i++, notifs.next().getNotification());
+            Notification notif = notifs.next().getNotification();
+            notif.extras.putChar("replayed", 'Y');
+            mNotifyMgr.notify(i++, notif);
         }
+        snoozedNotifs.clear();
     }
-
-    // Binder given to clients
-    private final IBinder mBinder = new LocalBinder();
-    // Random number generator
-    private final Random mGenerator = new Random();
-
-    /**
-     * Class used for the client Binder.  Because we know this service always
-     * runs in the same process as its clients, we don't need to deal with IPC.
-     */
-    public class LocalBinder extends Binder {
-        MyNotificationListenerService getService() {
-            // Return this instance of LocalService so clients can call public methods
-            return MyNotificationListenerService.this;
+    public void clearAllNotifs()
+    {
+        snoozedNotifs.clear();
+        cancelAllNotifications();
+    }
+    class NLServiceReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getStringExtra("command").equals("flush")){
+                MyNotificationListenerService.this.flushAllSnoozedNotifs();
+            } else if(intent.getStringExtra("command").equals("flush")){
+                MyNotificationListenerService.this.clearAllNotifs();
+            }
         }
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return mBinder;
     }
 }
