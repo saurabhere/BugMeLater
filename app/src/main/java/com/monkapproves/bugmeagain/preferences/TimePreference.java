99b16520b598316b1df9b080cb430196d5b3711d
ankitgupta <mailtoankitgupta@gmail.com>
2016-12-15 10:54:33 +0530

    Work hours Start/End persist in millis (long). Summary shown in Settings. Also defaultValue is set for Start/End

--- app/src/main/java/com/monkapproves/bugmeagain/preferences/TimePreference.java
+++ app/src/main/java/com/monkapproves/bugmeagain/preferences/TimePreference.java
@@ -3,48 +3,51 @@ package com.monkapproves.bugmeagain.preferences;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.preference.DialogPreference;
+import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TimePicker;
 
+import com.monkapproves.bugmeagain.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 //http://stackoverflow.com/questions/5533078/timepicker-in-preferencescreen
 public class TimePreference extends DialogPreference {
-    private int lastHour=0;
-    private int lastMinute=0;
-    private TimePicker picker=null;
+    private Calendar calendar;
+    private TimePicker picker = null;
 
-    public static int getHour(String time) {
-        String[] pieces=time.split(":");
-
-        return(Integer.parseInt(pieces[0]));
-    }
-
-    public static int getMinute(String time) {
-        String[] pieces=time.split(":");
-
-        return(Integer.parseInt(pieces[1]));
+    public TimePreference(Context ctxt) {
+        this(ctxt, null);
     }
 
     public TimePreference(Context ctxt, AttributeSet attrs) {
-        super(ctxt, attrs);
+        this(ctxt, attrs, android.R.attr.dialogPreferenceStyle);
+    }
 
-        setPositiveButtonText("Set");
-        setNegativeButtonText("Cancel");
+    public TimePreference(Context ctxt, AttributeSet attrs, int defStyle) {
+        super(ctxt, attrs, defStyle);
+
+        setPositiveButtonText(R.string.set);
+        setNegativeButtonText(R.string.cancel);
+        calendar = new GregorianCalendar();
     }
 
     @Override
     protected View onCreateDialogView() {
-        picker=new TimePicker(getContext());
-
-        return(picker);
+        picker = new TimePicker(getContext());
+        return (picker);
     }
 
     @Override
     protected void onBindDialogView(View v) {
         super.onBindDialogView(v);
-
-        picker.setCurrentHour(lastHour);
-        picker.setCurrentMinute(lastMinute);
+        picker.setCurrentHour(calendar.get(Calendar.HOUR_OF_DAY));
+        picker.setCurrentMinute(calendar.get(Calendar.MINUTE));
     }
 
     @Override
@@ -52,39 +55,45 @@ public class TimePreference extends DialogPreference {
         super.onDialogClosed(positiveResult);
 
         if (positiveResult) {
-            lastHour=picker.getCurrentHour();
-            lastMinute=picker.getCurrentMinute();
+            calendar.set(Calendar.HOUR_OF_DAY, picker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, picker.getCurrentMinute());
 
-            String time=String.valueOf(lastHour)+":"+String.valueOf(lastMinute);
-
-            if (callChangeListener(time)) {
-                persistString(time);
+            setSummary(getSummary());
+            if (callChangeListener(calendar.getTimeInMillis())) {
+                persistLong(calendar.getTimeInMillis());
+                notifyChanged();
             }
         }
     }
 
     @Override
     protected Object onGetDefaultValue(TypedArray a, int index) {
-        return(a.getString(index));
+        return (a.getString(index));
     }
 
     @Override
     protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-        String time=null;
 
         if (restoreValue) {
-            if (defaultValue==null) {
-                time=getPersistedString("00:00");
-            }
-            else {
-                time=getPersistedString(defaultValue.toString());
+            calendar.setTimeInMillis(getPersistedLong(System.currentTimeMillis()));
+        } else {
+            if (defaultValue != null) {
+                java.text.DateFormat df = new SimpleDateFormat("h:mm a");
+                try {
+                    calendar.setTime(df.parse((String) defaultValue));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        else {
-            time=defaultValue.toString();
-        }
+    }
 
-        lastHour=getHour(time);
-        lastMinute=getMinute(time);
+    @Override
+    public CharSequence getSummary() {
+        if (calendar == null) {
+            return null;
+        }
+        return DateFormat.getTimeFormat(getContext()).format(new Date(calendar.getTimeInMillis()));
     }
 }
+
